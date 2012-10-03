278ec024f626e11d1853560bf369f1f07e81bc84
Christian Kalhauge <christian@kalhauge.dk>
2012-10-03 13:18:28 +0200

    Added a README file and things to the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup,Extension
 
 setup(name='Anaconda',
+      url='https://github.com/kalhauge/Anaconda',
       version='0.1',
+      author='Christian Gram Kalhugae',
+      author_email='christian@kalhauge.dk',
       packages = ["Anaconda"],
       py_modules=[
          'Anaconda.AssociationMining',
