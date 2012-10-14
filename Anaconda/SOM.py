8d4063e3ef805ccb74b2ff56f4997dcb932d1017
Christian Kalhauge <christian@kalhauge.dk>
2012-10-14 16:06:20 +0200

    Added DrawHexagon Image

--- Anaconda/SOM.py
+++ Anaconda/SOM.py
@@ -72,41 +72,83 @@ class SOM:
       
       log.debug("Done training took {} s, speed {} hz".format(dur,iterations/dur));
    
+   @staticmethod
+   def drawHexagonImage(values,min_size):
+      scale = 128;
+      (x_size,y_size) = values.shape;
+      (xs,ys) = np.mgrid[0:x_size,0:y_size];
+      
+      # finding centers.
+      (c_xs,c_ys) = (xs*scale*0.8660254038 + scale ,ys*scale + scale);
+      
+      c_ys[0:y_size:2] = np.float64(c_ys[0:y_size:2]) - ( 0.25* scale)
+      c_ys[1:y_size:2] = np.float64(c_ys[1:y_size:2]) + ( 0.25* scale)
+      
+      (hexa_x, hexa_y) = ([0]*6,[0]*6);
+      (hexa_o_x, hexa_o_y) = ([0]*6,[0]*6);
+      for r in xrange(6):
+         hexa_o_x[r] = (c_xs + np.cos(r*np.pi/3)*scale/2/0.8560254038);
+         hexa_o_y[r] = (c_ys + np.sin(r*np.pi/3)*scale/2/0.8560254038);
+         hexa_x[r] = (c_xs + np.cos(r*np.pi/3)*scale/2/0.9);
+         hexa_y[r] = (c_ys + np.sin(r*np.pi/3)*scale/2/0.9);
+      
+      size = (int(x_size*scale* 0.8660254038+scale),y_size*scale+scale);
+      img = Image.new("L",size,255);
+      d = ImageDraw.Draw(img);
+      for (x,y) in np.nditer([xs,ys]):
+         a = []
+         for r in xrange(6):
+            a.append((hexa_o_x[r][x,y],hexa_o_y[r][x,y]));
+         
+         d.polygon(a,fill=0);
+         
+         a = []
+         for r in xrange(6):
+            a.append((hexa_x[r][x,y],hexa_y[r][x,y]));
+         
+         d.polygon(a,fill=values[x,y]*255);
+      
+      a = min(float(size[0])/min_size,float(size[1])/min_size);
+      size = (int(size[0]/a),int(size[1]/a));
+      
+      return img.resize(size,Image.ANTIALIAS)
+
+   
    def getGridAsImages(self,size):
       if self.isHexagon:
          log.debug("Outputing images with hexagons");
-         scale = 32;
-         imgs = [];
-         (xs,ys) = np.mgrid[0:self.size,0:self.size];
-         (p_xs,p_ys) = (xs*scale,ys*scale);
-         p_ys[0:self.size:2] = np.float64(p_ys[0:self.size:2]) - ( 0.25* scale)
-         p_ys[1:self.size:2] = np.float64(p_ys[1:self.size:2]) + ( 0.25* scale)
-#         p_xs = p_xs * 0.8660254038;
-         hexa_x = [0]*6
-         hexa_y = [0]*6
-         for r in xrange(6):
-            hexa_x[r] = (p_xs + np.cos(r*np.pi/3)*scale/2) + scale;
-            hexa_y[r] = (p_ys + np.sin(r*np.pi/3)*scale/2) + scale;
-         
-         for i in xrange(self.vector_size):
-            img = Image.new("L",(self.size*scale+scale,self.size*scale+scale));
-            d = ImageDraw.Draw(img);
-            for (x,y) in np.nditer([xs,ys]):
-               a = []
-               for r in xrange(6):
-                  a.append((hexa_x[r][x,y],hexa_y[r][x,y]));
-               d.polygon(a,fill=self.grid[x,y,i]*255);
-               
-            imgs.append(img.resize(size,Image.BICUBIC));
-               
+         return [SOM.drawHexagonImage(self.grid[:,:,i],size) for i in range(self.vector_size)]
       else:
          log.debug("Outputing images with squares");
          imgs = [];
          for i in xrange(self.vector_size):
             img = Image.fromarray(np.uint8(self.grid[:,:,i]*255));
             imgs.append(img.resize(size,Image.NEAREST));
-      return imgs;
-      
+         return imgs;
+
+   def getUMatrixImage(self,size):
+      if self.isHexagon:
+         log.debug("Outputing U-matrix with hexagons");
+         
+         (x,y,z) = self.grid[:,:,:].shape;
+         s = (x*2,y*2)
+         umatrix = np.zeros(s);
+         
+         xs,ys = np.mgrid[0:x-1,0:y];
+         
+         a = np.sqrt(np.sum((self.grid[xs,ys,:]-self.grid[xs+1,ys,:])**2,axis=2));
+         
+         umatrix[0,2:s[1]:4] = -1;
+         umatrix[1:s[0]:2,0:s[1]:2] = a;
+         
+         xs,ys = np.mgrid[0:x,0:y-1];
+         
+         print umatrix;
+         
+      else:
+         log.error("Outputing U-matrix with squares, is not allowed");
+         sys.exit(-1);
+
    
 class SOMTester(unittest.TestCase):
    
@@ -130,9 +172,20 @@ class SOMTester(unittest.TestCase):
    def testSimpleImages(self):
       self.som.createGrid(10,2,True);
       self.som.train([[0,0],[1,0],[0,1],[1,1]],1000);
-      imgs = self.som.getGridAsImages((512,512));
-      imgs[0].show();
-      imgs[1].show();
+      imgs = self.som.getGridAsImages(512);
+#      imgs[0].show();
+#      imgs[1].show();
+      
+   def testDrawHexagonImage(self):
+      self.som.createGrid(10,2,True);
+      self.som.train([[0,0],[1,0],[0,1],[1,1]],1000);
+      img = SOM.drawHexagonImage(self.som.grid[:,:,0],512);
+#      img.show();
+   
+   def testGetUMatrix(self):
+      self.som.createGrid(3,2,True);
+      self.som.train([[0,0],[1,0],[0,1],[1,1]],1000);
+      img = self.som.getUMatrixImage(512);
       
 if __name__ == "__main__":
    logging.basicConfig(level=logging.DEBUG);
