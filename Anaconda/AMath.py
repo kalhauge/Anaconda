9a8d915d2e74f9b1a0ac5ce93e6219d5a45a1686
Christian Kalhauge <christian@kalhauge.dk>
2012-11-20 11:45:20 +0100

    Upgradet math, specialy the euclideanSqDist which now finds all distaces

--- Anaconda/AMath.py
+++ Anaconda/AMath.py
@@ -1,8 +1,3 @@
-import math
-import sys
-import unittest
-import numpy as np
-
 """
 Name: AMath
 Author: Christian Gram Kalhauge : kalhauge@cbs.dtu.dk 
@@ -11,6 +6,33 @@ Date : 14th September - 2012
 Math is a collection of methods used in math.
 
 """
+
+import math
+import sys
+import unittest
+import numpy as np
+
+def calculateStatistics(vectors):
+   """
+   returns number of values pr column, the mean of each colloum
+   and the stdartd diviration of each column
+
+   returns (values_pr_coloum,mean,stddiv);
+   """
+   values_pr_column = (vectors.shape[0] - np.isnan(vectors).sum(0));
+
+   mean = np.nan_to_num(vectors).sum(0)
+   mean[values_pr_row != 0] /= values_pr_column[values_pr_column != 0];
+   mean[values_pr_row == 0] = np.nan;
+   
+   sqrdmean = (np.nan_to_num(vectors)**2).sum(0);
+   sqrdmean[values_pr_row != 0] /= values_pr_column[values_pr_column != 0];
+   sqrdmean[values_pr_row == 0] = np.nan;
+   
+   stddiv = np.sqrt(sqrdmean - mean**2)
+
+   return (values_pr_column,mean,stddiv);
+
 
 def gammalog(n):
    return math.log(math.factorial(n-1));
@@ -26,18 +48,19 @@ def hypergeometric(k,G,n,N):
    return math.exp(binomiallog(G,k) + binomiallog(N-G,n-k) - binomiallog(N,n));
 
 def euclideanSqDist(data,grid):
-   not_nan = ~np.isnan(data);
-   number = not_nan.sum();
-   # sys.stderr.write("{}\n".format(type(not_nan)));
-   # sys.stderr.write("{}\n".format(type(data)));
-   # sys.stderr.write("{}\n".format(type(grid)));
-   # 
-   # sys.stderr.write("{}\n".format(data[not_nan]));
-   # sys.stderr.write("{}\n".format(grid[...,not_nan]));
-   
-   ndir = data[not_nan] - grid[...,not_nan];
-   return np.sum(ndir*ndir,-1) * data.shape[0] / number;
+   """
+   Returns the distance from one np_array to all
+   vectors in the grid, the input is any compination of array and matrix
+   """
+   d = data if data.ndim == 2 else np.reshape(data,(-1,data.shape[-1]));
+   g = grid if grid.ndim == 2 else np.reshape(grid,(-1,grid.shape[-1]));
+   di,gi = np.mgrid[0:d.shape[0],0:g.shape[0]];
+   ndir = d[di,:] - g[gi,:];
+   number = np.sum(~np.isnan(ndir),-1);
+   return np.sum(np.nan_to_num(ndir*ndir),-1) * d.shape[-1] / number;
 
+
+   
 def gaussian(pos,matrix,shift=None):
  #   pos = pos.reshape((pos.shape[0],-1));
  #  
@@ -92,8 +115,13 @@ class MathTester (unittest.TestCase):
       self.assertTrue((euclideanSqDist(a,test2) == np.array([4.0,16.0,36.0])).all());
       
       test3 = np.array([[[0,1,24,3],[-1,0,24,2],[-2,-1,24,1]],[[0,1,24,3],[-1,0,24,2],[-2,-1,24,1]],[[0,1,24,3],[-1,0,24,2],[-2,-1,24,1]]]);
-      self.assertTrue((euclideanSqDist(a,test3) == np.array([[4.0,16.0,36.0],[4.0,16.0,36.0],[4.0,16.0,36.0]])).all());
-   
+      self.assertTrue((euclideanSqDist(a,test3) == np.array([[4.0,16.0,36.0,4.0,16.0,36.0,4.0,16.0,36.0]])).all());
+
+   def testEuclideanSqDistAddvanced(self):
+      a = np.array([[0,0,np.nan,0],[1,1,1,1]]);
+      test = np.array([[0.5,np.nan,0.5,0.5],[1.5,1.5,1.5,1.5]]);
+      print euclideanSqDist(a,test);
+      
    def testGaussian(self):
       array =np.mgrid[-1:2,-1:2];
 #      print array
--- Anaconda/Clustering.py
+++ Anaconda/Clustering.py
@@ -33,9 +33,68 @@ def associateDataWithCenters(data,centers,sqDistFunction=AMath.euclideanSqDist):
          output[index].append(i);
       return output;
 
-def getKmeansClusters(data):
-   pass;
+class AgglomerativeClusterAlgorithm:
+   """
+   The highest class for doing AgglomertiveClustering.
+   """
+   class AgglomerativeClusterNode:
+      def __init__(self,vectors,sqDistFunction,A=None,B=None):
+          """
+          Vectors is a matrix of vectores.
+          """
+          self.cluster = cluster;
+          self.size = vector.shape[0];
+          self.avg_dist = np.sum(np.sqrt(sqDistFunction(vector,vector))) / (self.size * self.size - self.size)
+          self.centroid = np.sum(vector) / self.size;
+          self.cent_dist = np.sum(np.sqrt(sqDistFunction(self.centroid,vector))) / self.size
+          self.A = A;
+          self.B = B;
+
+   def __init__(self):
+      setSqDistanceMethod(AMath.euclidianSqDist);
+
+   def setClusterFitnessMethod(self,clusterFitFunction):
+      """
+      Sets the method to determine the distance of two clusters.
+      the function needs to be of the form:
+       lambda cluster1, cluster2, sqdistfunction: real.
+      """
+      self._clusterFitFunction = clusterFitFunction;
    
+   def setSqDistanceMethod(self,sqDistFunction):
+      """
+      Sets the method for calculating the Squard Distance between two
+      methods, per default is it AMath's euclidianSqDist.
+      """
+      self._sqDistFunction = sqDistFunction;
+
+   def setStartingClusters(self,clusters):
+      """
+      Sets the begining clusters for the method. Clusters is an
+      iterative array of vectors. The sqDistanceMethod, needs to be
+      set before calling this method. 
+      """
+      self._startingClusters = [];
+      for cluster in clusters:
+          self._startingClusters.append(AgglomerativeClusterNode(vectors,self._sqDistFunction));
+
+   def createDendrogram(self):
+      """
+      Runs the Agglomerative clustering to create a tree, which the
+      user later can query. The original method uses a navive approach to
+      clustering. Runningtime O(n^3)..
+      """
+      C = list(self._startingClusters);
+
+      while len(C) >= 2:
+      best_pair = (self._clusterFitFunction(C[0],C[1]),C[0],C[1])
+      
+      for L in C:
+         for K in C:
+             if L != K:
+                
+                 
+         
    
    
    
